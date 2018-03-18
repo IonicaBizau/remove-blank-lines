bbeecba2896ce3f0ac6715ed87b514c319787f3f
renanbastos93 <renanbastos.tec@gmail.com>
2018-03-17 21:31:16 -0300

    created test scenario

---
+++ test/index.js
@@ -0,0 +1,31 @@
+"use strict";
+const assert = require("assert");
+const removeBlankLines = require("./../lib");
+
+const blankLines = new RegExp(/(^[ \t]*\n)/, "gm");
+const inputWithBlankLines = `
+Hello
+
+I'm a tester that want help yours texts.
+
+see ya.
+
+
+
+
+-> Amazing lib!!!
+`;
+const inputWithoutBlankLines = `Hello
+I'm a tester that want help yours texts.
+see ya.
+-> Amazing lib!!!
+`;
+
+describe("this project must clean the blank lines" , () => {
+    it("must blank lines this input" , () => {
+        assert.equal(blankLines.test(inputWithBlankLines), true);
+    });
+    it("String must equal" , () => {
+        assert.equal(removeBlankLines(inputWithBlankLines), inputWithoutBlankLines);
+    });
+});
